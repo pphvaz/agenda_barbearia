9c999ffc5a3fe31e58eac37a507e8240b982f4f7
Pedro Alves <pedrohvs.alves@gmail.com>
2024-07-29 17:59:50 -0300

    Criação Entidade Role

---
+++ src/main/java/pphvaz/barbearia/model/Role.java
@@ -0,0 +1,61 @@
+package pphvaz.barbearia.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="role")
+@SequenceGenerator(name = "seq_role",sequenceName = "seq_role",allocationSize = 1, initialValue = 1)
+public class Role implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_role")
+	private Long id;
+	
+	@Column(nullable = false)
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
