0308f7b7958c4eb0a5cf21ade275c591a2d8cc58
Pedro Alves <pedrohvs.alves@gmail.com>
2024-07-30 19:02:11 -0300

    Criação Entidade Forma de Pagamento

---
+++ src/main/java/pphvaz/barbearia/model/FormaDePagamento.java
@@ -0,0 +1,61 @@
+package pphvaz.barbearia.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "forma_de_pagamento")
+@SequenceGenerator(name = "seq_forma_de_pagamento", sequenceName = "seq_forma_de_pagamento", initialValue = 1, allocationSize = 1)
+public class FormaDePagamento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_forma_de_pagamento")
+	private Long id;
+
+	@Column(nullable = false)
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FormaDePagamento other = (FormaDePagamento) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
