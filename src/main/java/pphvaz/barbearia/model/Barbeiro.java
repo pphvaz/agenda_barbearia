f45dcec5797651c19f3d9661272c842af6a658c9
Pedro Alves <pedrohvs.alves@gmail.com>
2024-07-29 22:50:19 -0300

    Criado Anotação para Tabela Auxiliar Barbeiro_Servico

--- src/main/java/pphvaz/barbearia/model/Barbeiro.java
+++ src/main/java/pphvaz/barbearia/model/Barbeiro.java
@@ -1,12 +1,18 @@
 package pphvaz.barbearia.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name="barbeiro")
@@ -20,6 +26,15 @@ public class Barbeiro extends Pessoa {
 	@OneToOne
 	@JoinColumn(name = "agenda_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "agenda_fk"))
 	private Agenda agenda;
+	
+	@ManyToMany
+    @JoinTable(
+        name = "barbeiro_servico",
+		uniqueConstraints = @UniqueConstraint (columnNames = {"barbeiro_id", "servico_id"}, name = "unique_barbeiro_servico"),
+        joinColumns = @JoinColumn(name = "barbeiro_id", referencedColumnName = "id", table = "barbeiro", unique = false, foreignKey = @ForeignKey(name = "barbeiro_fk", value = ConstraintMode.CONSTRAINT)),
+        inverseJoinColumns = @JoinColumn(name = "servico_id", unique = false, referencedColumnName = "id", table = "servico", foreignKey = @ForeignKey(name = "servico_fk", value = ConstraintMode.CONSTRAINT))
+    )
+    private Set<Servico> servicos = new HashSet<>();
 
 	public Integer getAnosDeExperiencia() {
 		return anosDeExperiencia;
